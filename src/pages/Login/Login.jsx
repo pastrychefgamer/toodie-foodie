97533fcd86d594b86034eeec49f052e5f7c46794
William Aleshire <Mine@Williams-iMac.domain>
2020-04-13 16:29:05 -0700

    day 1 and day 2 complete

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../../components/LoginForm/Loginform';
+// import styles from './Login.module.css';
+
+const Login = (props) => {
+    return (
+        <main>
+            <h1>Login</h1>
+            <LoginForm />
+        </main>
+    );
+}
+
+export default Login;
---
+++ src/pages/Restaurants/Restaurants.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './Restaurants.module.css';
+
+const Restaurants = (props) => {
+    return (
+        <main>
+            <h1>Restaurants</h1>
+        </main>
+    );
+}
+
+export default Restaurants;
