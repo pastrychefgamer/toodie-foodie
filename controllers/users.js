7b69b5455ede2d21b66dc5f041969081ce89d867
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-05-04 13:44:31 -0700

    generating jwt now on signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.SECRET;
 
 module.exports = {
     signup
@@ -7,8 +10,17 @@ module.exports = {
 async function signup(req, res) {
     try {
         const user = await User.create(req.body);
-        res.json(user);
+        const token = createJWT(user);
+        res.json({ token });
     } catch (error) {
         res.status(400).json(error);
     }
+}
+
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        SECRET,
+        {expiresIn: '24h'}
+    );
 }
