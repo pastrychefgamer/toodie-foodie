d8adf792bba30021eb53d1249e94d23e3ab067a2
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-05-04 14:10:54 -0700

    adding user to state

---
+++ src/utils/tokenService.js
@@ -0,0 +1,13 @@
+function setToken(token) {
+    if(token) {
+        localStorage.setItem('token', token);
+    } else {
+        localStorage.removeItem('token');
+    }
+}
+
+
+
+export default {
+    setToken
+}
--- src/utils/userService.js
+++ src/utils/userService.js
@@ -1,21 +1,21 @@
-
+import tokenService from './tokenService';
 
 const BASE_URL = '/api/users/';
 
 function signup(user) {
     return fetch(BASE_URL + 'signup', {
         method: 'POST',
-        headers: new Headers({ 'Content-type': 'Application/json' }),
+        headers: new Headers({'Content-type': 'Application/json'}),
         body: JSON.stringify(user)
     })
     .then(response => {
-        if (response.ok) {
+        if(response.ok) {
             return response.json()
         } else {
             throw new Error('Email Already Taken!')
         }
     })
-        .then(data => console.log(data))
+    .then(({ token }) => tokenService.setToken(token))
 }
 
 export default {
