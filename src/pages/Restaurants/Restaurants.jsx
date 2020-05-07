598500f1fb80244a8732d0d7c4016810d6d928ea
pastrychefgamer <52508868+pastrychefgamer@users.noreply.github.com>
2020-05-07 15:02:50 -0700

    hook for hiding/showing form

--- src/pages/Restaurants/Restaurants.jsx
+++ src/pages/Restaurants/Restaurants.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RestaurantForm from '../../components/RestaurantForm/RestaurantForm';
 // import styles from './Restaurants.module.css';
 
 const Restaurants = (props) => {
+    const [ formVisible, setVisibility ] = useState(false);
     return (
         <main>
             <h1>Restaurants</h1>
+            <button onClick={() => setVisibility(!formVisible)}>
+                {formVisible ? 'Hide Form' : 'Show Form'}
+            </button>
             {
                 props.restaurants.map(({title, _id, cuisine, addedBy} ) => (
                 <section key={_id}>
@@ -15,7 +19,12 @@ const Restaurants = (props) => {
                 </section>
                 ))
             }
-            <RestaurantForm {...props} />
+            {
+                formVisible
+                &&
+                <RestaurantForm {...props} />
+            }
+            
         </main>
     );
 }
